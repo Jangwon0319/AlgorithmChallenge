242982890216d2369002b3dd6fd31fd245224082
JangWon Ryu <157569592+Jangwon0319@users.noreply.github.com>
2024-08-31 16:18:23 +0900

    [Silver II] Title: 유기농 배추, Time: 132 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -3,62 +3,60 @@ import java.util.*;
 
 public class Main {
 
+    static int M, N, K;
     static int[][] farm;
     static boolean[][] visited;
-    static int m, n, k;
-    static int[] dx = {-1, 1, 0, 0}; // 상하좌우 이동을 위한 배열
+    static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static int nx, ny;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());    // 테스트 케이스 개수
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());   // 가로 길이
+            N = Integer.parseInt(st.nextToken());   // 세로 길이
+            K = Integer.parseInt(st.nextToken());   // 배추의 위치 개수
+
+            // 배추의 위치 초기화
+            farm = new int[M][N];
+            visited = new boolean[M][N];
+
+            for (int j = 0; j < K; j++) {
+                StringTokenizer location = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(location.nextToken());
+                int y = Integer.parseInt(location.nextToken());
+                farm[x][y] = 1;
+            }
+
+            int wormCount = 0;
+
+            for (int j = 0; j < M; j++) {
+                for (int k = 0; k < N; k++) {
+                    if (farm[j][k] == 1 && !visited[j][k]) {
+                        wormCount++;
+                        DFS(j, k);
+                    }
+                }
+            }
+            System.out.println(wormCount);
+        }
+    }
 
     public static void DFS(int x, int y) {
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            nx = x + dx[i];
+            ny = y + dy[i];
 
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && farm[nx][ny] == 1) {
+            if (nx >= 0 && ny >= 0 && nx < M && ny < N && !visited[nx][ny] && farm[nx][ny] == 1) {
                 DFS(nx, ny);
             }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int t = Integer.parseInt(br.readLine()); // 테스트 케이스의 개수
-
-        for (int tc = 0; tc < t; tc++) {
-            st = new StringTokenizer(br.readLine());
-            m = Integer.parseInt(st.nextToken()); // 배추밭의 가로 길이
-            n = Integer.parseInt(st.nextToken()); // 배추밭의 세로 길이
-            k = Integer.parseInt(st.nextToken()); // 배추가 심어져 있는 위치의 개수
-
-            farm = new int[m][n];
-            visited = new boolean[m][n];
-
-            // 배추 위치 입력받기
-            for (int i = 0; i < k; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                farm[x][y] = 1; // 배추가 심어져 있는 위치를 1로 표시
-            }
-
-            int wormCount = 0; // 필요한 벌레 수 (배추 그룹의 수)
-
-            // 모든 위치를 순회하면서 배추 그룹 찾기
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (farm[i][j] == 1 && !visited[i][j]) { // 배추가 있고 방문하지 않았으면
-                        DFS(i, j); // DFS로 연결된 모든 배추 방문
-                        wormCount++; // 새로운 배추 그룹 발견
-                    }
-                }
-            }
-
-            // 결과 출력
-            System.out.println(wormCount);
         }
     }
 }
